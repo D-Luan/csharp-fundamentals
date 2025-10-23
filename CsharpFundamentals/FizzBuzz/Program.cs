1f355c11da0d68404ce4d4c840941a99aa152a9f
d-luan <dluan1285@gmail.com>
2025-10-23 15:57:51 -0300

    feat: add FizzBuzz solution

---
+++ CsharpFundamentals/FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FizzBuzz;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 20;
+        List<string> res = FizzBuzz(n);
+
+        foreach (string s in res)
+        {
+            Console.Write($"{s} ");
+        }
+    }
+
+    public static List<string> FizzBuzz(int n)
+    {
+        List<string> res = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            string s = "";
+
+            if (i % 3 == 0)
+            {
+                s += "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                s += "Buzz";
+            }
+
+            if (s == "")
+            {
+                s += i.ToString();
+            }
+
+            res.Add(s);
+        }
+
+        return res;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Csharp Fundamentals
+
+A list of problem solving about C# fundamentals.
+
+### Solved Problems
+
+| Problem | Solution | Key Concept Used | Time Complexity | Space Complexity |
+| --- | --- | --- | --- | --- |
+| [Fizz Buzz](https://www.geeksforgeeks.org/dsa/fizz-buzz-implementation/#by-string-concatenation) | [Code](./CsharpFundamentals/FizzBuzz/Program.cs) | Basic loops, conditionals, and the modulo operator. | **O(n)** | **O(n)**
