4336858fb70eceff675020b875207223660a4049
d-luan <dluan1285@gmail.com>
2025-11-29 13:38:24 -0300

    feat(headfirst): add complete game code

---
+++ HeadFirstCsharp/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+        foreach(int specificNumber in numbers)
+        {
+            Console.WriteLine($"Especific number: {specificNumber}");
+        }
+    }
+}
